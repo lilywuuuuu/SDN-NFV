f4c1ba2eddb27d3c163e67408ec84f0d6ebedce8
Lily Wu <20021212lilywu@gmail.com>
2024-11-11 19:15:57 +0800

    [Demo3] first commit

---
+++ Demo3/no-packet-in/src/main/java/nycu/winlab/groupmeter/AppComponent.java
@@ -0,0 +1,382 @@
+package nycu.winlab.groupmeter;
+
+import static org.onosproject.net.config.NetworkConfigEvent.Type.CONFIG_ADDED;
+import static org.onosproject.net.config.NetworkConfigEvent.Type.CONFIG_UPDATED;
+import static org.onosproject.net.config.basics.SubjectFactories.APP_SUBJECT_FACTORY;
+
+import org.onlab.packet.ARP;
+import org.onlab.packet.Ethernet;
+import org.onlab.packet.IPv4;
+import org.onlab.packet.Ip4Address;
+import org.onlab.packet.MacAddress;
+
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.core.GroupId;
+
+import org.onosproject.net.flow.DefaultFlowRule;
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.flow.DefaultTrafficTreatment;
+import org.onosproject.net.flow.FlowRule;
+import org.onosproject.net.flow.FlowRuleService;
+
+import org.onosproject.net.packet.OutboundPacket;
+import org.onosproject.net.packet.DefaultOutboundPacket;
+import org.onosproject.net.packet.PacketService;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketContext;
+import org.onosproject.net.packet.PacketPriority;
+import org.onosproject.net.packet.InboundPacket;
+
+import org.onosproject.net.config.ConfigFactory;
+import org.onosproject.net.config.NetworkConfigEvent;
+import org.onosproject.net.config.NetworkConfigListener;
+import org.onosproject.net.config.NetworkConfigRegistry;
+import org.onosproject.net.ConnectPoint;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.FilteredConnectPoint;
+import org.onosproject.net.PortNumber;
+
+import org.onosproject.net.group.Group;
+import org.onosproject.net.group.DefaultGroupBucket;
+import org.onosproject.net.group.DefaultGroupDescription;
+import org.onosproject.net.group.GroupBucket;
+import org.onosproject.net.group.GroupBuckets;
+import org.onosproject.net.group.GroupDescription;
+import org.onosproject.net.group.GroupService;
+
+import org.onosproject.net.meter.MeterRequest;
+import org.onosproject.net.meter.MeterService;
+import org.onosproject.net.meter.Band;
+import org.onosproject.net.meter.DefaultBand;
+import org.onosproject.net.meter.DefaultMeterRequest;
+import org.onosproject.net.meter.Meter;
+import org.onosproject.net.meter.MeterId;
+
+import org.onosproject.net.intent.IntentService;
+import org.onosproject.net.intent.PointToPointIntent;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Collection;
+
+@Component(immediate = true)
+public class AppComponent {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private final HostConfigListener cfgListener = new HostConfigListener();
+    private final ConfigFactory<ApplicationId, HostConfig> factory = new ConfigFactory<ApplicationId, HostConfig>(
+            APP_SUBJECT_FACTORY, HostConfig.class, "informations") {
+        @Override
+        public HostConfig createConfig() {
+            return new HostConfig();
+        }
+    };
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected NetworkConfigRegistry cfgService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected CoreService coreService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected PacketService packetService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected FlowRuleService flowRuleService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected GroupService groupService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected MeterService meterService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected IntentService intentService;
+
+    private GroupMeterIntentProcessor processor = new GroupMeterIntentProcessor();
+    private int flowPriority = 50000;
+    private ApplicationId appId;
+    private ConnectPoint h1, h2, h3;
+    private MacAddress mac1, mac2, mac3;
+    private Ip4Address ip1, ip2, ip3;
+
+    @Activate
+    protected void activate() {
+        appId = coreService.registerApplication("nycu.winlab.groupmeter");
+
+        // config listener
+        cfgService.addListener(cfgListener);
+        cfgService.registerConfigFactory(factory);
+
+        // packet processor
+        packetService.addProcessor(processor, PacketProcessor.director(2));
+
+        // install a flowrule for packet-in
+        TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
+        selector.matchEthType(Ethernet.TYPE_IPV4);
+        packetService.requestPackets(selector.build(), PacketPriority.REACTIVE, appId);
+
+        log.info("Started");
+    }
+
+    @Deactivate
+    protected void deactivate() {
+        // remove flowrule installed by app
+        flowRuleService.removeFlowRulesById(appId);
+
+        // remove config listerner
+        cfgService.removeListener(cfgListener);
+        cfgService.unregisterConfigFactory(factory);
+
+        // remove packet processor
+        packetService.removeProcessor(processor);
+        processor = null;
+
+        // remove flowrule installed for packet-in
+        TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
+        selector.matchEthType(Ethernet.TYPE_IPV4);
+        packetService.cancelPackets(selector.build(), PacketPriority.REACTIVE, appId);
+
+        log.info("Stopped");
+    }
+
+    private class HostConfigListener implements NetworkConfigListener {
+        @Override
+        public void event(NetworkConfigEvent event) {
+            if ((event.type() == CONFIG_ADDED || event.type() == CONFIG_UPDATED)
+                    && event.configClass().equals(HostConfig.class)) {
+                HostConfig config = cfgService.getConfig(appId, HostConfig.class);
+                if (config != null) {
+                    // get information from config
+                    h1 = ConnectPoint.deviceConnectPoint(config.host1());
+                    h2 = ConnectPoint.deviceConnectPoint(config.host2());
+                    h3 = ConnectPoint.deviceConnectPoint(config.host3());
+                    mac1 = MacAddress.valueOf(config.mac1());
+                    mac2 = MacAddress.valueOf(config.mac2());
+                    mac3 = MacAddress.valueOf(config.mac3());
+                    ip1 = Ip4Address.valueOf(config.ip1());
+                    ip2 = Ip4Address.valueOf(config.ip2());
+                    ip3 = Ip4Address.valueOf(config.ip3());
+
+                    // log information
+                    log.info("ConnectPoint_h1: {}, ConnectPoint_h2: {}, ConnectPoint_h3: {}", h1, h2, h3);
+                    log.info("MacAddress_h1: {}, MacAddress_h2: {}, MacAddress_h3: {}", mac1, mac2, mac3);
+                    log.info("IpAddress_h1: {}, IpAddress_h2: {}, IpAddress_h3: {}", ip1, ip2, ip3);
+
+                    // group entry & flowrule for s1
+                    GroupId groupId = installGroup(h1.deviceId());
+                    flowGroup(h1.deviceId(), groupId);
+
+                    // meter entry & flowrule for s4
+                    DeviceId devId4 = DeviceId.deviceId("of:0000000000000004");
+                    MeterId meterId = installMeter(devId4);
+                    flowMeter(devId4, meterId);
+
+                    // intent service between h1 and h3, h2 and h3
+                    createIntent(h3, h1, mac1);
+                    createIntent(h1, h3, mac3);
+                    createIntent(h3, h2, mac2);
+                    createIntent(h2, h3, mac3);
+                }
+            }
+        }
+
+        private void createIntent(ConnectPoint srcCP, ConnectPoint dstCP, MacAddress matchMac) {
+            FilteredConnectPoint ingress = new FilteredConnectPoint(srcCP);
+            FilteredConnectPoint egress = new FilteredConnectPoint(h1);
+
+            TrafficSelector selector = DefaultTrafficSelector.builder()
+                    .matchEthDst(matchMac)
+                    .build();
+
+            PointToPointIntent intent = PointToPointIntent.builder()
+                    .filteredIngressPoint(ingress)
+                    .filteredEgressPoint(egress)
+                    .selector(selector)
+                    .priority(flowPriority)
+                    .appId(appId)
+                    .build();
+
+            intentService.submit(intent);
+            log.info("Intent `{}`, port `{}` => `{}`, port `{}` is submitted.",
+                    srcCP.deviceId(), srcCP.port(), dstCP.deviceId(), dstCP.port());
+        }
+
+        private GroupId installGroup(DeviceId devId) {
+            // bucket 1: outPort = 2 and watchPort = 2
+            TrafficTreatment treatment1 = DefaultTrafficTreatment.builder()
+                    .setOutput(PortNumber.portNumber(2))
+                    .build();
+            GroupBucket bucket1 = DefaultGroupBucket.createFailoverGroupBucket(
+                    treatment1, PortNumber.portNumber(2), GroupId.valueOf(0));
+
+            // bucket 2: outPort = 3 and watchPort = 3
+            TrafficTreatment treatment2 = DefaultTrafficTreatment.builder()
+                    .setOutput(PortNumber.portNumber(3))
+                    .build();
+            GroupBucket bucket2 = DefaultGroupBucket.createFailoverGroupBucket(
+                    treatment2, PortNumber.portNumber(3), GroupId.valueOf(0));
+
+            GroupBuckets buckets = new GroupBuckets(Arrays.asList(bucket1, bucket2));
+            GroupDescription groupDescription = new DefaultGroupDescription(
+                    devId, GroupDescription.Type.FAILOVER, buckets);
+
+            groupService.addGroup(groupDescription);
+            Group group = groupService.getGroups(devId).iterator().next();
+            return group.id();
+        }
+
+        private void flowGroup(DeviceId devId, GroupId groupId) {
+            TrafficSelector selector = DefaultTrafficSelector.builder()
+                    .matchInPort(PortNumber.portNumber(1))
+                    .matchEthType(Ethernet.TYPE_IPV4)
+                    .build();
+
+            TrafficTreatment treatment = DefaultTrafficTreatment.builder()
+                    .group(groupId)
+                    .build();
+
+            FlowRule flowRule = DefaultFlowRule.builder()
+                    .forDevice(devId)
+                    .withSelector(selector)
+                    .withTreatment(treatment)
+                    .withPriority(flowPriority)
+                    .makePermanent()
+                    .fromApp(appId)
+                    .build();
+
+            flowRuleService.applyFlowRules(flowRule);
+            log.info("Flow rule installed on device {} with groupID {}", devId, groupId);
+        }
+
+        private MeterId installMeter(DeviceId devId) {
+            // create a drop band
+            Band dropBand = DefaultBand.builder()
+                    .ofType(Band.Type.DROP)
+                    .withRate(512) // Rate in KB per second
+                    .burstSize(1024) // Burst size
+                    .build();
+            Collection<Band> bands = Collections.singletonList(dropBand);
+
+            MeterRequest meterRequest = DefaultMeterRequest.builder()
+                    .forDevice(devId)
+                    .withUnit(Meter.Unit.KB_PER_SEC)
+                    .withBands(bands)
+                    .burst() // set burst to true
+                    .fromApp(appId)
+                    .add();
+
+            Meter meter = meterService.submit(meterRequest);
+            return (MeterId) meter.meterCellId();
+        }
+
+        private void flowMeter(DeviceId devId, MeterId meterId) {
+            TrafficSelector selector = DefaultTrafficSelector.builder()
+                    .matchEthSrc(mac1)
+                    .build();
+
+            TrafficTreatment treatment = DefaultTrafficTreatment.builder()
+                    .setOutput(PortNumber.portNumber(2))
+                    .meter(meterId)
+                    .build();
+
+            FlowRule flowRule = DefaultFlowRule.builder()
+                    .forDevice(devId)
+                    .withSelector(selector)
+                    .withTreatment(treatment)
+                    .withPriority(flowPriority)
+                    .makePermanent()
+                    .fromApp(appId)
+                    .build();
+
+            flowRuleService.applyFlowRules(flowRule);
+            log.info("Flow rule installed on device {} with meterID {}", devId, meterId);
+        }
+    }
+
+    private class GroupMeterIntentProcessor implements PacketProcessor {
+        @Override
+        public void process(PacketContext context) {
+            if (context.isHandled())
+                return;
+
+            InboundPacket pkt = context.inPacket();
+            ConnectPoint srcCP = pkt.receivedFrom();
+            DeviceId recDevId = srcCP.deviceId();
+            PortNumber recPort = srcCP.port();
+            Ethernet ethPkt = pkt.parsed();
+            if (ethPkt == null)
+                return;
+
+            if (ethPkt.getEtherType() == Ethernet.TYPE_ARP) { // deal with ARP packet
+                log.info("ARP packet");
+                ARP arpPkt = (ARP) ethPkt.getPayload();
+                if (arpPkt.getOpCode() == ARP.OP_REQUEST) {
+                    Ip4Address dstIp = Ip4Address.valueOf(arpPkt.getTargetProtocolAddress());
+                    MacAddress dstMac = dstIp.equals(ip1) ? mac1 : mac2;
+                    Ethernet arpReply = ARP.buildArpReply(dstIp, dstMac, ethPkt);
+                    packetOut(ByteBuffer.wrap(arpReply.serialize()), recDevId, recPort);
+                }
+            } else { // install intent service for IPv4 packet
+                log.info("IPv4 packet");
+                IPv4 ipv4Pkt = (IPv4) ethPkt.getPayload();
+                int dstIP = ipv4Pkt.getDestinationAddress();
+                FilteredConnectPoint ingress = new FilteredConnectPoint(srcCP);
+                FilteredConnectPoint egress1 = new FilteredConnectPoint(h1);
+                FilteredConnectPoint egress2 = new FilteredConnectPoint(h2);
+
+                // intent service for h2 to h1
+                if (dstIP == ip1.toInt()) {
+                    handleIntent(srcCP, h1, mac1, ingress, egress1);
+                    packetOut(pkt.unparsed(), h1.deviceId(), h1.port());
+                }
+                // intent service for (s2 or s4) to h2
+                else if (dstIP == ip2.toInt()) {
+                    handleIntent(srcCP, h2, mac2, ingress, egress2);
+                    packetOut(pkt.unparsed(), h2.deviceId(), h2.port());
+                }
+            }
+        }
+
+        private void packetOut(ByteBuffer pkt, DeviceId recDevId, PortNumber recPort) {
+            TrafficTreatment treatment = DefaultTrafficTreatment.builder()
+                    .setOutput(recPort)
+                    .build();
+            OutboundPacket packet = new DefaultOutboundPacket(recDevId, treatment, pkt);
+            packetService.emit(packet);
+        }
+
+        private void handleIntent(ConnectPoint srcCP, ConnectPoint dstCP, MacAddress matchMac,
+                FilteredConnectPoint ingress, FilteredConnectPoint egress) {
+
+            TrafficSelector selector = DefaultTrafficSelector.builder()
+                    .matchEthDst(matchMac)
+                    .build();
+
+            PointToPointIntent intent = PointToPointIntent.builder()
+                    .filteredIngressPoint(ingress)
+                    .filteredEgressPoint(egress)
+                    .selector(selector)
+                    .priority(flowPriority)
+                    .appId(appId)
+                    .build();
+
+            intentService.submit(intent);
+            log.info("Intent `{}`, port `{}` => `{}`, port `{}` is submitted.",
+                    srcCP.deviceId(), srcCP.port(), dstCP.deviceId(), dstCP.port());
+        }
+    }
+}
---
+++ Demo3/no-packet-in/src/main/java/nycu/winlab/groupmeter/HostConfig.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nycu.winlab.groupmeter;
+
+import org.onlab.packet.MacAddress;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.net.ConnectPoint;
+import org.onosproject.net.config.Config;
+
+public class HostConfig extends Config<ApplicationId> {
+
+  public static final String HOST1 = "host-1";
+  public static final String HOST2 = "host-2";
+  public static final String HOST3 = "host-3";
+  public static final String MAC1 = "mac-1";
+  public static final String MAC2 = "mac-2";
+  public static final String MAC3 = "mac-3";
+  public static final String IP1 = "ip-1";
+  public static final String IP2 = "ip-2";
+  public static final String IP3 = "ip-3";
+
+  @Override
+  public boolean isValid() {
+    return hasOnlyFields(HOST1, HOST2, HOST3, MAC1, MAC2, MAC3, IP1, IP2, IP3);
+  }
+
+  public String host1() {
+    return get(HOST1, null);
+  }
+
+  public String host2() {
+    return get(HOST2, null);
+  }
+
+  public String host3() {
+    return get(HOST3, null);
+  }
+
+  public String mac1() {
+    return get(MAC1, null);
+  }
+
+  public String mac2() {
+    return get(MAC2, null);
+  }
+
+  public String mac3() {
+    return get(MAC3, null);
+  }
+
+  public String ip1() {
+    return get(IP1, null);
+  }
+
+  public String ip2() {
+    return get(IP2, null);
+  }
+
+  public String ip3() {
+    return get(IP3, null);
+  }
+}
